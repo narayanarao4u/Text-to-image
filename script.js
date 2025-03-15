f3b960b1d1a121080c4c0251cfda82508ace0484
Narayana Rao <narayanarao4u@yahoo.com>
2025-03-15 03:56:22 +0000

    final with out API Key

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const selectCount = document.querySelector('#select-count');
 const selectRatio = document.querySelector('#select-ratio');
 const galleryGrid = document.querySelector('.gallery-grid');
 
+
+const API_KEY = 'API_KEY'; // Replace with your Hugging Face API key
+
+
+
+
 const examplePrompts = [
     "A magic forest with glowing plants and fairy homes among giant mushrooms",
     "An old steampunk airship floating through golden clouds at sunset",
@@ -53,10 +59,110 @@ promptInput.focus();
  
 })
 
-const CreateImageCard = (model, count, ratio, promptText) => {
-  for (let i = 0; i < count; i++) {
+let tt = `
+                        <div class="img-overlay">
+                            <div class="img-download-btn">
+                                <i class="fa-solid fa-download"></i>
+                            </div>
+                        </div>
+`
+
+const getImageDimensions = (aspectRatio, baseSize = 512) => { 
+  
+    const [width, height] = aspectRatio.split("/").map(Number);
+    const scaleFactor = baseSize / Math.sqrt(width * height);
+
+    let calculatedWidth = Math . round(width * scaleFactor);
+    let calculatedHeight = Math. round(height * scaleFactor);
     
+    // Ensure dimensions are multiples of 16 (AI model requirements)
+    calculatedWidth = Math. floor(calculatedWidth / 16) * 16;
+    calculatedHeight = Math. floor(calculatedHeight / 16) * 16;
+    
+    return { width: calculatedWidth, height: calculatedHeight }
+}
+
+const updateImageCard = (index, ImgURL) => {
+  const imgCard = document.getElementById(`img-card-${index}`);
+
+  if(!imgCard) {
+    return;
   }
+
+  imgCard.classList.remove('loading');
+  imgCard.classList.remove('error');
+
+  imgCard.innerHTML = ` 
+  <img src="${ImgURL}" alt="" class="result-img">
+   <div class="img-overlay">
+      <a  href="${ImgURL}"  download="${Date.now()}.png" class="img-download-btn">
+          <i class="fa-solid fa-download"></i>
+      </a>
+  </div>
+  
+  `
+
+
+}
+const generateImage =  async (model, count, ratio, promptText) => {
+  const MODEL_URL = `https://router.huggingface.co/hf-inference/models/${model}`
+
+  const { width, height } = getImageDimensions(ratio);
+
+  const imagePromises = Array.from({ length: count }, async (_, i) => {
+
+      try {
+        const response = await fetch(MODEL_URL, {
+          headers: {
+            Authorization: `Bearer ${API_KEY}`,
+            "Content-Type": "application/json",
+            "x-use-cache": "false",
+          },
+          method: "POST",
+          body: JSON.stringify({
+            inputs: promptText,
+            parameters: {width, height},
+            options : {wait_for_model: true, user_cache: false}
+          }),
+
+        }) ;
+
+        if(!response.ok) {
+          throw new Error((await response.json())?.error);
+        }
+
+        const result = await response.blob();
+        updateImageCard(i, URL.createObjectURL(result));
+        
+
+      } catch (error) {
+        console.error(error);
+      }
+
+
+
+  });
+
+ await Promise.allSettled(imagePromises);
+
+}
+const CreateImageCard = (model, count, ratio, promptText) => {
+  galleryGrid.innerHTML = ''; // Clear the gallery grid
+  for (let i = 0; i < count; i++) {
+    galleryGrid.innerHTML += `
+                       <div class="img-card loading" id="img-card-${i}" 
+                        style="aspect-ratio: ${ratio};">
+                        <div class="status-container ">
+                            <div class="spinner"></div>
+                            <i class="fa-solid fa-triangle-exclamation" style="font-size: 1.75rem; color: #ef4444;"></i>
+                            <p class="status-text">Generating . . .</p>
+                        </div>
+                       
+                    </div>
+    `
+  }
+
+  generateImage(model, count, ratio, promptText); // Generate images using the selected model, count, ratio, and prompt text
 }
 
 promptForm.addEventListener('submit', (e) => {
